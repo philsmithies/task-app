b24cb398b3689762879343f80e6e42e7630aaf0e
Phil Smithies <philsmithies@gmail.com>
2021-07-07 10:47:16 +0200

    basic to do tasks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Overview from './components/Overview'
+import uniqid from "uniqid";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      tasks: [],
+      task: { 
+        text: '',
+        id: uniqid()
+    },
+    }
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      task: {
+        text: e.target.value,
+        id: this.state.task.id,
+      }
+    })
+  }
+
+  onSubmitTask = (e) => {
+    e.preventDefault()
+    this.setState({
+      tasks: this.state.tasks.concat(this.state.task),
+      task: { 
+        text: '',
+        id: uniqid()
+      }
+    })
+  }
+
+  render(){
+    const { tasks, task } = this.state
+
+    return (
+      <div className="App">
+        <h2>Hello and welcome to the task master</h2>
+        <form onSubmit={this.onSubmitTask}>
+          <input 
+          onChange={this.handleChange}
+          value={task.text}
+          id="taskInput" 
+          type="text"/>
+          <button type="submit">
+            Submit
+          </button>
+        </form>
+        <Overview tasks={tasks}/>
+      </div>
+    );
+  }
 }
 
 export default App;
